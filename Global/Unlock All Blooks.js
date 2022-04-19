47f82be45ba7971d05344874ea5e8caa09f272ae
Matty <53088136+byronbytes@users.noreply.github.com>
2022-04-19 11:05:38 -0400

    bookmarklets are supposed to be one line
    can someone test this

--- Global/Unlock All Blooks.js
+++ Global/Unlock All Blooks.js
@@ -1,4 +1,1 @@
-let e = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner
-e.stateNode.state.lockedBlooks = []
-alert("Select a diffrent blook to unlock all blooks")
-alert("coded by zastix#0001")
+let e = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner e.stateNode.state.lockedBlooks = [] alert("Select a different blook to unlock all blooks.") alert("coded by zastix#0001")
