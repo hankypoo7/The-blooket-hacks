4f1bf36ef8814f2f9bac83aa35e604edd1f714ee
VillainsRule <101288516+VillainsRule4000@users.noreply.github.com>
2022-06-12 18:53:30 -0400

    Dumb thing

--- Utils/NoDelay.js
+++
@@ -1,3 +0,0 @@
-javascript:setTimeout=function(f,t){f()};
-
-// What this does is make it so that you don't have to wait to open boxes in like crypto hack, wait for getting a fish in fishing frenzy. I don't know how to describe it, so you can test it if you want
