5d2678a8dbf8a48d5b3ef1b3c529ac434ccac786
Disease <83523587+CPScript@users.noreply.github.com>
2023-12-12 14:19:35 -0500

    Add files via upload

---
+++ Global/Unlock All Blooks.js
@@ -0,0 +1,4 @@
+let e = Object.values(document.querySelector('#app > div > div'))[1].children[1]._owner
+e.stateNode.state.lockedBlooks = []
+alert("Select a diffrent blook to unlock all blooks")
+alert("coded by zastix#0001")
