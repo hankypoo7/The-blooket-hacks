a8800597be7c8d6c4108781b825e5dc2641db5e7
Hankypoo7 <100436822+hankypoo7@users.noreply.github.com>
2023-12-13 13:24:30 -0600

    deobfusticate

---
+++ Global/xpTokens.js
@@ -0,0 +1,94 @@
+(async () = > {
+    let n =  document.createElement('iframe');
+    document.body.append(n);
+    window.alert =  n.contentWindow.alert.bind(window);
+    window.prompt =  n.contentWindow.prompt.bind(window);
+    window.confirm =  n.contentWindow.confirm.bind(window);
+    n.remove();
+
+   var getValues =  () = > new Promise(
+   (e,  t) = > {
+
+      try {
+          let n =  window.webpackJsonp.map(e = > Object.keys(e[1]).map(t = > e[1]
+         [t])).reduce(
+         (e,  t) = > [.e,  .t],  []).find(e = > / \w {
+            8
+         } - \w {
+            4
+         } - \w {
+            4
+         } - \w {
+            4
+         } - \w {
+            12
+         }
+         /.test(e.toString()) && /\ (new TextEncoder\)\.encode\ (\"(.+?)\"\)/.test(e.toString())).toString();
+            e({
+                blooketBuild: n.match(/\w{8}-\w{4}-\w{4}-\w{4}-\w{12}/)[0],
+                secret: n.match(/\(new TextEncoder\)\.encode\(\"(.+?)\"\)/)[1]
+            })
+        } catch {
+            t("
+         Could not fetch auth details ")
+        }
+    })
+    var encodeValues = async (e, t) => {
+        let d = window.crypto.getRandomValues(new Uint8Array(12));
+        return window.btoa(Array.from(d).map(e => String.fromCharCode(e)).join("") + Array.from(new Uint8Array(await window.crypto.subtle.encrypt({
+            name: "
+         AES - GCM ",
+            iv: d
+        }, await window.crypto.subtle.importKey("
+         raw ", await window.crypto.subtle.digest("
+         SHA - 256 ", (new TextEncoder).encode(t)), {
+            name: "
+         AES - GCM "
+        }, !1, ["
+         encrypt "]), (new TextEncoder).encode(JSON.stringify(e))))).map(e => String.fromCharCode(e)).join(""))
+    };
+            fetch("
+         https: //api.blooket.com/api/users", { credentials: "include" }).then(x => x.json()).then(x => {
+          getValues().then(async e = > {
+             fetch("https://api.blooket.com/api/users/add-rewards",  {
+                method:  "put",
+                credentials:  "include",
+                headers:  {
+                   "content-type":  "application/json",
+                   "X-Blooket-Build":  e.blooketBuild
+               },
+                body:  await encodeValues({
+                   name:  x.name,
+                   addedTokens:  500,
+                   addedXp:  300
+               },
+                e.secret)
+            });
+             fetch("https://api.blooket.com/api/users/add-rewards",  {
+                method:  "put",
+                credentials:  "include",
+                headers:  {
+                   "content-type":  "application/json",
+                   "X-Blooket-Build":  e.blooketBuild
+               },
+                body:  await encodeValues({
+                   name:  x.name,
+                   addedTokens:  500,
+                   addedXp:  300
+               },
+                e.secret)
+            }).then(
+            () = > alert('Added daily rewards!')).
+            catch(
+            () = > alert('There was an error when adding rewards!'));;
+
+         }).
+         catch(
+         () = > alert('There was an error encoding requests!'));
+
+      }).
+      catch(
+      () = > alert('There was an error getting username!'));
+   })
+      });
+   }; footer();
